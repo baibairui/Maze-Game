ce34ca4343ba0281282443f112a589fc2f34da89
wys <CST2209167@xmu.edu.my>
2024-06-16 19:01:52 +0800

    僵尸和豌豆图片已上传

--- src/main/java/com/edu/xmum/cst206/View/Entity/V1/AiView.java
+++ src/main/java/com/edu/xmum/cst206/View/Entity/V1/AiView.java
@@ -3,6 +3,8 @@ package com.edu.xmum.cst206.View.Entity.V1;
 import com.edu.xmum.cst206.Model.Direction;
 import com.edu.xmum.cst206.Model.Interface.IPlayerModel;
 import com.edu.xmum.cst206.View.Interface.IPlayerView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -10,17 +12,25 @@ import javafx.scene.shape.Circle;
 public class AiView extends Pane implements IPlayerView {
     private int cellSize;
     private IPlayerModel player;
+    private Direction direction;
     public AiView(IPlayerModel playerModel){
         this.player=playerModel;
+        this.direction=Direction.DOWN;//默认向下
     }
 
     @Override
     public void draw() {
-        Circle circle=new Circle(cellSize/2, Color.GRAY);
-        circle.setCenterX(player.getX() * cellSize + cellSize / 2);
-        circle.setCenterY(player.getY() * cellSize + cellSize / 2);
+
+
+        Image playImg = new Image("com/edu/xmum/cst206/player/僵尸5.gif");
+        ImageView playerView = new ImageView(playImg);
+        playerView.setFitHeight(cellSize);
+        playerView.setFitWidth(cellSize); // 修正这里，应该设置宽度而不是再设置一次高度
+        playerView.setX(player.getX() * cellSize);
+        playerView.setY(player.getY() * cellSize);
+
         getChildren().clear();
-        getChildren().add(circle);
+        getChildren().add(playerView);
     }
 
     @Override
@@ -38,9 +48,9 @@ public class AiView extends Pane implements IPlayerView {
         return this;
     }
 
-    //空实现，不需要
     @Override
     public void setDirection(Direction direction) {
-
+        this.direction=direction;
+        draw();
     }
 }
